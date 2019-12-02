1a117079b469b7d845bc90ac87685d63d126a4da
imoans <naomins0101@gmail.com>
2019-12-02 22:27:19 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,16 @@ import {
   FunctionExpression,
 } from 'estree'
 
-export const rule: Rule.RuleModule = {
+/**
+ * dispatchが第一引数の関数のスコープまたは、dispatch関数内で、
+ * $path関数の引数が重複しないようにするルール
+ */
+const rule: Rule.RuleModule = {
   create: context => {
-    let argumentCountsByFunctionId: {
-      [functionId: string]: { [key: string]: number }
+    let argumentsById: {
+      [id: string]: string[][]
     } = {}
-    let functionIdStack: string[] = []
+    let idStack: string[] = []
 
     const onStartFunction = (node: Node) => {
       const { params } = node as
@@ -22,15 +26,25 @@ export const rule: Rule.RuleModule = {
         | FunctionExpression
 
       if (params.length === 0) return
-      if (params[0].type !== 'Identifier') return
-      if (params[0].name !== 'dispatch') return
+      const firstParam = params[0]
+      if (firstParam.type !== 'Identifier') return
+      if (firstParam.name !== 'dispatch') return
 
       const { line, column } = node.loc!.start
-      functionIdStack.push(`function${line}-${column}`)
+      idStack.push(`function${line}-${column}`)
     }
 
-    const onEndFunction = () => {
-      functionIdStack.pop()
+    const onEndFunction = (node: Node) => {
+      const { params } = node as
+        | ArrowFunctionExpression
+        | FunctionDeclaration
+        | FunctionExpression
+
+      if (params.length === 0) return
+      const firstParam = params[0]
+      if (firstParam.type !== 'Identifier') return
+      if (firstParam.name !== 'dispatch') return
+      idStack.pop()
     }
 
     return {
@@ -40,37 +54,58 @@ export const rule: Rule.RuleModule = {
       'ArrowFunctionExpression:exit': onEndFunction,
       'FunctionDeclaration:exit': onEndFunction,
       'FunctionExpression:exit': onEndFunction,
+      'CallExpression:exit': (node: Node) => {
+        const { callee } = node as CallExpression
+        if (callee.type !== 'Identifier') return
+        if (callee.name === 'dispatch') {
+          idStack.pop()
+          return
+        }
+      },
       CallExpression: node => {
         const { callee, arguments: args } = node as CallExpression
         if (callee.type !== 'Identifier') return
+
+        if (callee.name === 'dispatch') {
+          const { line, column } = node.loc!.start
+          idStack.push(`dispatch${line}-${column}`)
+          return
+        }
+
         if (callee.name !== '$path') return
-        const paths = args
+        const path = args
           .filter<Literal>(
             (argument): argument is Literal => argument.type === 'Literal',
           )
-          .map(({ value }) => value)
-        let firstPath = paths[0] as string
-        if (firstPath.includes('.')) {
-          firstPath = firstPath.split('.')[0]
-        }
+          .map(({ value }) => value) as string[]
+        const id = idStack.slice(-1)[0]
+        if (id == null) return
+        const currentArgs = argumentsById[id] || []
+        argumentsById[id] = [...currentArgs, path]
+        const includes = currentArgs.filter(arg => isIncludedPath(arg, path))
 
-        const functionId = functionIdStack.slice(-1)[0]
-        const argumentCounts = argumentCountsByFunctionId[functionId]
-
-        const count = (argumentCounts ? argumentCounts[firstPath] || 0 : 0) + 1
-
-        argumentCountsByFunctionId[functionId] = {
-          ...argumentCounts,
-          [firstPath]: count,
-        }
-
-        if (count > 1) {
+        if (includes.length > 0) {
           context.report({
             node,
-            message: `${firstPath} is specified ${count} times in the argument of $path method`,
+            message: `${path[0]} is specified ${includes.length +
+              1} times in the argument of $path method`,
           })
         }
       },
     }
   },
 }
+
+const isIncludedPath = (a: string[], b: string[]) => {
+  const sortedByLength = [a, b].sort((c, d) => c.length - d.length)
+  const short = sortedByLength[0].slice()
+  const long = sortedByLength[1]
+  let index = 0
+  while (short.length > 0 && long[index] === short[index]) {
+    short.pop()
+    index++
+  }
+  return short.length === 0
+}
+
+export = rule
